6dce0f56a9ff4bd7ba96cf30e40d3f6572fda825
Sandina Rajapaksha <tech.sandina@gmail>
2026-09-08 00:15:22 +0530

    chore(ui): custom name on starter ui

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,23 +1,44 @@
-  return {
-      {
-    "goolord/alpha-nvim",
-    -- dependencies = { 'nvim-mini/mini.icons' },
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      local startify = require("alpha.themes.startify")
-      -- available: devicons, mini, default is mini
-      -- if provider not loaded and enabled is true, it will try to use another provider
-      startify.file_icons.provider = "devicons"
-      require("alpha").setup(
-        startify.config
-      )
-    end,
-},
-    {
-        'goolord/alpha-nvim',
-        config = function ()
-            require'alpha'.setup(require'alpha.themes.dashboard'.config)
-        end
-    }
-  }
+return {
+  "goolord/alpha-nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
+    -- YOUR CURRENT LOGO
+    dashboard.section.header.val = {
+      [[                                   ███  ███                       ]],
+      [[                                  ▒███  ▒▒▒                       ]],
+      [[  █████   ██████   ████████    ███████  ████  ████████    ██████  ]],
+      [[ ███▒▒   ▒▒▒▒▒███ ▒▒███▒▒███  ███▒▒███ ▒▒███ ▒▒███▒▒███  ▒▒▒▒▒███ ]],
+      [[▒▒█████   ███████  ▒███ ▒███ ▒███ ▒███  ▒███  ▒███ ▒███   ███████ ]],
+      [[ ▒▒▒▒███ ███▒▒███  ▒███ ▒███ ▒███ ▒███  ▒███  ▒███ ▒███  ███▒▒███ ]],
+      [[ ██████ ▒▒████████ ████ █████▒▒████████ █████ ████ █████▒▒████████ ]],
+      [[▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒ ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒▒▒ ▒▒▒▒▒ ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒▒▒ ]],
+    }
+    -- buttons / footer you can keep default
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("r", "󰄉  Recent", ":Telescope oldfiles <CR>"),
+      dashboard.button("q", "󰅚  Quit", ":qa<CR>"),
+    }
+
+    -- custom layout = this is where you inject the name
+    dashboard.config.layout = {
+      { type = "padding", val = 2 },
+      dashboard.section.header,
+      { type = "padding", val = 1 },
+      custom_name,
+      { type = "padding", val = 2 },
+      dashboard.section.buttons,
+      { type = "padding", val = 1 },
+      dashboard.section.footer,
+    }
+
+    -- if you want startify icons provider
+    -- dashboard.section.footer.opts.hl = "Comment"
+
+    alpha.setup(dashboard.config)
+  end,
+}
