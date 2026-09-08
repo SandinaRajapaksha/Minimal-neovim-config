1581a8496352b49fba7e396045d0e453206d5749
Sandina Rajapaksha <tech.sandina@gmail>
2026-09-08 18:07:48 +0530

    some changes

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,14 +1,15 @@
-return { 'nvim-mini/mini.nvim', version = '*',
-config = function () 
-    require("mini.pairs").setup() 
-    require("mini.tabline").setup() 
-    -- require("mini.statusline").setup() 
-    require("mini.files").setup() 
-    require("mini.git").setup() 
-    require("mini.move").setup() 
-    require("mini.clue").setup() 
-    require("mini.indentscope").setup({symbol = "▏"}) 
-    vim.keymap.set("n","<leader>e",":lua MiniFiles.open()<CR>", {desc = "Open explorer"})
-end 
+return {
+	"nvim-mini/mini.nvim",
+	version = "*",
+	config = function()
+		require("mini.pairs").setup()
+		require("mini.tabline").setup()
+		-- require("mini.statusline").setup()
+		require("mini.files").setup()
+		require("mini.git").setup()
+		require("mini.move").setup()
+		require("mini.clue").setup()
+		require("mini.indentscope").setup({ symbol = "▏" })
+		vim.keymap.set("n", "<leader>e", ":lua MiniFiles.open()<CR>", { desc = "Open explorer" })
+	end,
 }
-
