5e2ad165ed174ff46deee8243a68cfd2ce3eb53a
Sandina Rajapaksha <tech.sandina@gmail>
2026-09-08 14:54:27 +0530

    feat(lsp): mason-lsp, nvim-lsp-config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,36 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"clangd",
+				"eslint",
+				"pyre",
+			},
+			-- auto calls vim.lsp.enable() for installed servers
+			automatic_enable = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Only put custom settings here. Defaults come from nvim-lspconfig itself.
+			-- Example:
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+					},
+				},
+			})
+			-- No need to call vim.lsp.enable() - mason-lspconfig does it for you
+			-- because automatic_enable = true
+		end,
+	},
 }
